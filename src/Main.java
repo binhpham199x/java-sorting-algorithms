6987cb020085edf479ebff96c764135ccac632ce
Binh Pham <binh.pham@synaigy.com>
2024-03-21 16:39:29 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+   public static void main(String[] args) {
+      int min = 0;
+      int max = 50;
+      int size = 20;
+
+
+      int[] numbers = generateIntArr(size, min,max);
+      System.out.println("numbers: "+Arrays.toString(numbers));
+
+
+
+
+   }
+
+   public static int[] generateIntArr(int size, int min, int max) {
+      int[] res = new int[size];
+
+      for (int i = 0; i < size; i++) {
+         res[i] = (int) (Math.random() * (max - min + 1) + min);
+      }
+
+      return res;
+   }
+}
